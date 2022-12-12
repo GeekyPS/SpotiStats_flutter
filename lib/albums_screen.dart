e98cddf0f2669131bb5fe50762543c3d2cb7a4c7
Tushar98644 <tusharbanik@Tushars-MacBook-Air.local>
2022-12-12 16:50:02 +0530

    latest

--- lib/albums_screen.dart
+++ lib/albums_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter_complete_guide/album_item.dart';
 import 'package:flutter_complete_guide/dummy_data2.dart';
 
 class AlbumsScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Container(
